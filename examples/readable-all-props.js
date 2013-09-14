a0d9a042668a483630844d1269ee8f25bd33a6f2
Thorsten Lorenz <thlorenz@gmx.de>
2013-09-14 17:53:03 -0400

    adding readable all props example

--- examples/readable-all-props.js
+++ examples/readable-all-props.js
@@ -3,8 +3,8 @@
 var numbers = require('../test/fixtures/number-readable');
 var nebraska = require('../');
 
-var readable = numbers({ to: 3, interval: 1000 })
-var numbersState = nebraska(readable, { interval: 500 })
+var readable = numbers({ to: 3, interval: 1000})
+var numbersState = nebraska(readable, { interval: 500, readable: nebraska.properties.readable })
 
 readable
   .on('end', numbersState.endSoon.bind(numbersState))
